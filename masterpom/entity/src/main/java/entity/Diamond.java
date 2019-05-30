95afd48647cfdc23c3bce8f73c16d881ce3cd0d3
inesbahraoui <ines.bahraoui@viacesi.fr>
2019-05-30 22:56:43 +0200

    commit

--- masterpom/entity/src/main/java/entity/Diamond.java
+++ masterpom/entity/src/main/java/entity/Diamond.java
@@ -1,5 +1,13 @@
 package entity;
 
-public class Diamond {
+public class Diamond extends Mobile{
 
+	/* The Sprite corresponding */
+	
+	private static final Sprite sprite = new Sprite('D', Sprite.mapTileSet, new Rectangle(64, 0, 16, 16));
+
+	/*private static final IStrategy strategy = new DiamondStrategy();*/
+	
+	
+	
 }

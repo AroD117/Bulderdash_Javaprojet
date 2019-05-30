55f319f4526d40f9eda6b5867c6cbf83d8571989
inesbahraoui <ines.bahraoui@viacesi.fr>
2019-05-30 18:23:18 +0200

    commit

--- masterpom/entity/src/main/java/entity/Mobile.java
+++ masterpom/entity/src/main/java/entity/Mobile.java
@@ -22,8 +22,8 @@ import fr.exia.insanevehicles.model.element.Sprite;
 	     *
 	     * @param sprite
 	     *            the sprite
-	     * @param road
-	     *            the road
+	     * @param map
+	     *            the map
 	     * @param permeability
 	     *            the permeability
 	     */
@@ -44,39 +44,48 @@ import fr.exia.insanevehicles.model.element.Sprite;
 	     *            the y
 	     * @param sprite
 	     *            the sprite
-	     * @param road
-	     *            the road
+	     * @param map
+	     *            the map
 	     * @param permeability
 	     *            the permeability
 	     */
-	    Mobile(final int x, final int y, final Sprite sprite, final IRoad road, final Permeability permeability) {
-	        this(sprite, road, permeability);
+	    Mobile(final int x, final int y, final Sprite sprite, final IMap map, final Permeability permeability) {
+	        this(sprite, map, permeability);
 	        this.setX(x);
 	        this.setY(y);
+	        
 	    }
+	    
+	    @Override
 		public void moveUp() {
 			this.setY(this.getY() - 1);
 			this.setHasMoved();
 
 	}
-
+	    
+	    @Override
 		public void moveDown() {
 			  this.setY(this.getY() + 1);
 		      this.setHasMoved();
 
 	}
 
+	    @Override
 		public void moveLeft() {
 			 this.setX(this.getX() - 1);
 		     this.setHasMoved();
 		
 	}
+	   
 
+	    @Override
 		public void moveRight() {
 			this.setX(this.getX() + 1);
 	        this.setHasMoved();
 	}
-		
+	    
+	   
+	    @Override
 		public void DoNothing() {
 			this.setHasMoved();
 		}
@@ -85,6 +94,7 @@ import fr.exia.insanevehicles.model.element.Sprite;
 			this.getMap().setMobileHasChanged();
 		}
 		
+		@Override
 		public int GetX() {
 			return this.getPosition().x;
 		}
@@ -95,12 +105,13 @@ import fr.exia.insanevehicles.model.element.Sprite;
 	            this.die();
 		}
 		
+	    @Override
 		public int GetY() {
 			return this.getPosition().y;
 		}
 		
 		public void SetY(int y) {
-			   this.getPosition().y = (y + this.getMap().getHeight()) % this.getMap().getHeight();
+			   this.getPosition().y = y;
 		       if (this.isCrashed()) {
 		       this.die();
 		}
@@ -113,7 +124,7 @@ import fr.exia.insanevehicles.model.element.Sprite;
 	        this.map = map;
 	    }
 		
-		
+		@Override
 		public boolean isAlive() {
 			 return this.alive;
 		}
@@ -127,7 +138,7 @@ import fr.exia.insanevehicles.model.element.Sprite;
 			return this.getMap().getOnTheMapXY(this.getX(), this.getY()).getPermeability() == Permeability.BLOCKING;
 		}
 		
-		public boolean isWon() {
+		/*public boolean isWon() {
 			return true;
 		}
 		
@@ -139,7 +150,7 @@ import fr.exia.insanevehicles.model.element.Sprite;
 			return true;
 		}
 		
-		/* public boolean isDesappear() {
+		public boolean isDesappear() {
 			return true;
 		}*/
 		
@@ -151,7 +162,67 @@ import fr.exia.insanevehicles.model.element.Sprite;
 	        this.position = position;
 	    }
 		
+		protected boolean mapAllowsMovementTo(final UserOrder direction) {
+			switch (direction) {
+			case UP:
+				return this.getMap().getOnTheMapXY(this.getX(), this.getY() - 1)
+						.getPermeability() == Permeability.PENETRABLE;
+			case DOWN:
+				return this.getMap().getOnTheMapXY(this.getX(), this.getY() + 1)
+						.getPermeability() == Permeability.PENETRABLE;
+			case RIGHT:
+				return this.getMap().getOnTheMapXY(this.getX() + 1, this.getY())
+						.getPermeability() == Permeability.PENETRABLE;
+			case LEFT:
+				return this.getMap().getOnTheMapXY(this.getX() - 1, this.getY())
+						.getPermeability() == Permeability.PENETRABLE;
+			case NOP:
+			default:
+				return true;
+			}
+		
+		
+			protected Boolean pawnsAllowMovementTo(final UserOrder direction) {
+				Point desiredPosition = this.getPositionFromUserOrder(direction);
+				for (IMobile pawn : this.getMap().getPawns()) {
+					if (pawn.getPosition().equals(desiredPosition)) {
+						if (pawn.getPermeability() != Permeability.PENETRABLE) {
+							return false;
+						} else {
+							return true;
+						}
+					}
+				}
+
+				return true;
+			}
+			
+			protected Point getPositionFromUserOrder(final UserOrder direction){
+				Point desiredPosition = null;
+				switch (direction) {
+				case UP:
+					desiredPosition = new Point(this.getX(), this.getY() - 1);
+					break;
+				case DOWN:
+					desiredPosition = new Point(this.getX(), this.getY() + 1);
+					break;
+				case RIGHT:
+					desiredPosition = new Point(this.getX() + 1, this.getY());
+					break;
+				case LEFT:
+					desiredPosition = new Point(this.getX() - 1, this.getY());
+					break;
+				case NOP:
+				default:
+					desiredPosition = new Point(this.getX(), this.getY());
+					break;
+				}
+				
+				return desiredPosition;
+			}
+			
+			
 	}
 
 
-}
+

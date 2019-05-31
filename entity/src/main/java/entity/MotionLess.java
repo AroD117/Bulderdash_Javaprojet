a8444102b9e023a3503692c1d9c5a6e0a4f36a97
LANGLOISGuillaume <49404944+LANGLOISGuillaume@users.noreply.github.com>
2019-05-31 11:02:18 +0200

    I forgot the modification about motionless class

--- entity/src/main/java/entity/MotionLess.java
+++ entity/src/main/java/entity/MotionLess.java
@@ -2,6 +2,9 @@ package entity;
 
 public abstract class MotionLess extends Entity{
 	
+	//Number for the map
+	protected static int num = 1; 
+	
 	//Default constructor
 	public MotionLess(Sprite sprite, Permability permability) {
 		super(sprite, permability);
